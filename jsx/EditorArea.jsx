ee561651e8f22999f022ddd7f9fa0e2a5b7bb8c4
fmihel <fmihel76@gmail.com>
2024-12-17 16:51:57 +0300

    data indexes

--- jsx/EditorArea.jsx
+++ jsx/EditorArea.jsx
@@ -18,12 +18,22 @@ import EditorTags from './EditorTags.jsx';
 import Html from '../utils/Html.js';
 import HtmlSpecialChars, { CR_CHAR, CR_HTML } from './js/HtmlSpecialChars.js';
 import scroll from './js/scroll.js';
+import DataWrap from './js/DataWrap.js';
 
 const buffer = {
     selects: [],
 };
 
-let process = false;
+let _lockKey = false;
+
+const lockKey = (handler) => {
+    if (!_lockKey) {
+        _lockKey = true;
+        handler();
+        setTimeout(() => { _lockKey = false; }, 0);
+    }
+};
+
 function EditorArea({
     // list = test_list,
     data: outerData,
@@ -39,6 +49,8 @@ function EditorArea({
     const [showCursor, setShowCursor] = useState(false);
     const [shiftSelect, setShiftSelect] = useState([]);
     const [mouseSelect, setMouseSelect] = useState(false);
+    const wrap = new DataWrap(data);
+
     // const [copy, setCopy] = useState([]);
     // let copy = [];
     // const setCopy = (newCopy) => {
@@ -112,13 +124,14 @@ function EditorArea({
     }, [shiftSelect, cursor, mouseSelect, outerSelect]);
 
     useEffect(() => {
-        setData([...outerData, End.createData()]);
+        setData(wrap.change(([...outerData, End.createData()])));
     }, [outerData]);
 
     useEffect(() => {
         if (onCursor) {
             onCursor({ cursor });
         }
+        scrollToViewPort();
     }, [cursor]);
 
     const doClickTag = (o) => {
@@ -140,13 +153,14 @@ function EditorArea({
             setCursor(last.id);
         }
     };
+
     const doMouseDown = () => {
         ref.current.focus();
         if (shiftSelect.length) {
             setShiftSelect([]);
             setCursor(false);
         } else {
-            reposCursor();
+            // reposCursor();
         }
     };
 
@@ -157,177 +171,153 @@ function EditorArea({
     };
 
     const doChange = (newData) => {
-        // if (onChange) {
-        //     onChange(newData.filter((it) => it.id !== 'end'));
-        // } else {
-        setData(newData);
-        // }
+        if (onChange) {
+            const out = [...newData];
+            if (newData.length && newData[newData.length - 1].id === 'end') {
+                out.pop();
+            }
+            onChange(wrap.change(out));
+        } else {
+            setData(wrap.change(newData));
+        }
     };
-
+    const scrollToViewPort = () => {
+        scroll.toViewPort('.editor-area', '.cursor', { margin: 32 });
+    };
     const doKeyDown = (o) => {
-        if (process) {
-            o.preventDefault();
-            return false;
-        }
+        lockKey(() => {
+            // console.log(o.key, o.keyCode, 'ctrl', o.ctrlKey, 'shift', o.shiftKey, selects_debug(selects));
+            // console.log(o.key, o.keyCode);
+            // const index = data.findIndex((it) => it.id === cursor);
+            const index = wrap.index(cursor);
+            let no_handler = true;
 
-        // console.log(o.key, o.keyCode, 'ctrl', o.ctrlKey, 'shift', o.shiftKey, selects_debug(selects));
-        // console.log(o.key, o.keyCode);
-        const index = data.findIndex((it) => it.id === cursor);
-        let no_handler = true;
+            // const symbols = [KEY_CODE_SPACE];
+            if (cursor && !o.ctrlKey) {
+                if (isCharKey(o.keyCode)) {
+                    no_handler = false;
 
-        // const symbols = [KEY_CODE_SPACE];
-        if (cursor && !o.ctrlKey) {
-            if (isCharKey(o.keyCode)) {
+                    doChange([
+                        ...data.slice(0, index),
+                        EditorTags.createData('char', { value: o.key }),
+                        ...data.slice(index)]);
+                }
+                if (o.keyCode === KEY_CODE_SPACE) {
+                    no_handler = false;
+
+                    doChange([
+                        ...data.slice(0, index),
+                        EditorTags.createData('space'),
+                        ...data.slice(index)]);
+                }
+            }
+            if (o.ctrlKey) {
                 no_handler = false;
 
-                doChange([
-                    ...data.slice(0, index),
-                    EditorTags.createData('char', { value: o.key }),
-                    ...data.slice(index)]);
-            }
-            if (o.keyCode === KEY_CODE_SPACE) {
-                no_handler = false;
-
-                doChange([
-                    ...data.slice(0, index),
-                    EditorTags.createData('space'),
-                    ...data.slice(index)]);
-            }
-        }
-        if (o.ctrlKey) {
-            no_handler = false;
-
-            if (o.keyCode === KEY_CODE_C) {
+                if (o.keyCode === KEY_CODE_C) {
                 // setCopy(getSelects());
-                navigator.clipboard.writeText(Data.asArray(getSelectsObjects(), (it) => it.value).join(''));
+                    navigator.clipboard.writeText(Data.asArray(getSelectsObjects(), (it) => it.value).join(''));
+                }
+                if (o.keyCode === KEY_CODE_V && cursor) {
+                    navigator.clipboard
+                        .readText()
+                        .then((clipText) => {
+                            const newData = Html.toData(HtmlSpecialChars.shield(clipText).replaceAll(CR_CHAR, CR_HTML))
+                                .map((it) => ({
+                                    ...it,
+                                    ...it.value ? { value: HtmlSpecialChars.unShield(it.value) } : {},
+                                }));
+
+                            doChange(Data.insert(data, newData, cursor));
+                        });
+                }
+
+                if (o.keyCode === KEY_CODE_A) {
+                    setShiftSelect(Data.ids(data).filter((it) => it.id !== 'end'));
+                }
             }
-            if (o.keyCode === KEY_CODE_V && cursor) {
-                navigator.clipboard
-                    .readText()
-                    .then((clipText) => {
-                        const newData = Html.toData(HtmlSpecialChars.shield(clipText).replaceAll(CR_CHAR, CR_HTML))
-                            .map((it) => ({
-                                ...it,
-                                ...it.value ? { value: HtmlSpecialChars.unShield(it.value) } : {},
-                            }));
 
-                        doChange(Data.insert(data, newData, cursor));
-                    });
+            if (o.keyCode === KEY_CODE_ENTER) { // enter
+                no_handler = false;
+                doChange([
+                    ...data.slice(0, index),
+                    EditorTags.createData('br'),
+                    ...data.slice(index)]);
             }
 
-            if (o.keyCode === KEY_CODE_A) {
-                setShiftSelect(Data.ids(data).filter((it) => it.id !== 'end'));
+            if (o.keyCode === KEY_CODE_LEFT && cursor) { // to left
+                no_handler = false;
+                const prev = wrap.prev(cursor);
+                if (prev) {
+                    setCursor(prev.id);
+                }
+                setShiftSelect(o.shiftKey ? array.addUnique(shiftSelect, prev.id) : []);
             }
-        }
 
-        if (o.keyCode === KEY_CODE_ENTER) { // enter
-            no_handler = false;
-            doChange([
-                ...data.slice(0, index),
-                EditorTags.createData('br'),
-                ...data.slice(index)]);
-        }
-
-        if (o.keyCode === KEY_CODE_LEFT) { // to left
-            no_handler = false;
-            const prev = Data.prev(data, (it) => eq.id(it.id, cursor));
-            if (prev) {
-                setCursor(prev.id);
+            if (o.keyCode === KEY_CODE_RIGHT && cursor) { // to right
+                no_handler = false;
+                const next = wrap.next(cursor);
+                if (next) {
+                    setCursor(next.id);
+                }
+                setShiftSelect(o.shiftKey ? array.addUnique(shiftSelect, cursor) : []);
             }
-            setShiftSelect(o.shiftKey ? array.addUnique(shiftSelect, prev.id) : []);
-        }
 
-        if (o.keyCode === KEY_CODE_RIGHT) { // to right
-            process = true;
-            no_handler = false;
-            console.time('check');
-            Data.nextAsync(data, (it) => eq.id(it.id, cursor))
-                .then((next) => {
-                    if (next) {
-                        setCursor(next.id);
-                    }
-                    setShiftSelect(o.shiftKey ? array.addUnique(shiftSelect, cursor) : []);
-                    // setShiftSelect(o.shiftKey ? array.addUnique(shiftSelect, cursor) : []);
-                    scroll.toViewPort('.editor-area', '.cursor', { margin: 32 });
-                    process = false;
-                    console.timeEnd('check');
-                });
-            // const next = Data.next(data, (it) => eq.id(it.id, cursor));
-            // if (next) {
-            //     setCursor(next.id);
+            if (o.keyCode === KEY_CODE_UP && cursor) { // to up
+                no_handler = false;
+                const up = wrap.nearest(cursor, (it) => it.type === 'br');
+                setShiftSelect([]);
+                setCursor(up ? up.id : (data.length ? data[0].id : 0));
+            }
+
+            if (o.keyCode === KEY_CODE_DOWN && cursor) { // to down
+                no_handler = false;
+                const down = wrap.nearest(cursor, (it) => it.type === 'br', false);
+
+                setShiftSelect([]);
+                setCursor(down ? down.id : (data.length ? data[data.length - 1].id : 0));
+            }
+
+            if (o.keyCode === KEY_CODE_BACKSPACE) { // backspace
+                no_handler = false;
+                const sel = getSelects();
+                if (sel.length) {
+                    const next = Data.next(data, (it) => eq.id(it.id, sel[sel.length - 1]));
+
+                    doChange(data.filter((it) => !sel.find((sid) => eq.id(it.id, sid))));
+                    setShiftSelect([]);
+                    setCursor(next ? next.id : 0);
+                } else {
+                    const prev = Data.prev(data, (it) => eq.id(it.id, cursor));
+                    // console.log({ prev });
+                    doChange(data.filter((it) => !eq.id(it.id, prev.id)));
+                }
+            }
+
+            if (o.keyCode === KEY_CODE_DEL) { // delete
+                no_handler = false;
+                const sel = getSelects();
+                if (sel.length) {
+                    const next = Data.next(data, (it) => eq.id(it.id, sel[sel.length - 1]));
+
+                    doChange(data.filter((it) => !sel.find((sid) => eq.id(it.id, sid))));
+                    setShiftSelect([]);
+                    setCursor(next ? next.id : 0);
+                } else {
+                    const next = Data.next(data, (it) => eq.id(it.id, cursor));
+                    doChange(data.filter((it) => !eq.id(it.id, cursor)));
+                    setCursor(next ? next.id : 0);
+                }
+            }
+
+            // if ([KEY_CODE_LEFT, KEY_CODE_RIGHT, KEY_CODE_UP, KEY_CODE_DOWN,KEY_].indexOf(o.keyCode) > -1 || isCharKey(o.keyCode)) {
+            // scroll.toViewPort('.editor-area', '.cursor', { margin: 32 });
+            // if (scr) console.log({ scr });
             // }
-            // setShiftSelect(o.shiftKey ? array.addUnique(shiftSelect, cursor) : []);
-        }
-
-        if (o.keyCode === KEY_CODE_UP) { // to up
-            no_handler = false;
-            process = true;
-            Data.nearestAsync(data, (it) => eq.id(it.id, cursor), (it) => it.type === 'br')
-                .then((up) => {
-                    setShiftSelect([]);
-                    setCursor(up ? up.id : (data.length ? data[0].id : 0));
-                    scroll.toViewPort('.editor-area', '.cursor', { margin: 32 });
-                    process = false;
-                });
-        }
-
-        if (o.keyCode === KEY_CODE_DOWN) { // to down
-            no_handler = false;
-            process = true;
-            Data.nearestAsync(data, (it) => eq.id(it.id, cursor), (it) => it.type === 'br', false)
-                .then((down) => {
-                    setShiftSelect([]);
-                    setCursor(down ? down.id : (data.length ? data[data.length - 1].id : 0));
-                    scroll.toViewPort('.editor-area', '.cursor', { margin: 32 });
-                    process = false;
-                });
-
-            // no_handler = false;
-            // const down = Data.nearest(data, (it) => eq.id(it.id, cursor), (it) => it.type === 'br', false);
-            // setShiftSelect([]);
-            // setCursor(down ? down.id : (data.length ? data[data.length - 1].id : 0));
-            // process = true;
-        }
-
-        if (o.keyCode === KEY_CODE_BACKSPACE) { // backspace
-            no_handler = false;
-            const sel = getSelects();
-            if (sel.length) {
-                const next = Data.next(data, (it) => eq.id(it.id, sel[sel.length - 1]));
-
-                doChange(data.filter((it) => !sel.find((sid) => eq.id(it.id, sid))));
-                setShiftSelect([]);
-                setCursor(next ? next.id : 0);
-            } else {
-                const prev = Data.prev(data, (it) => eq.id(it.id, cursor));
-                // console.log({ prev });
-                doChange(data.filter((it) => !eq.id(it.id, prev.id)));
+            if (no_handler) {
+                console.log('no handler');
             }
-        }
-
-        if (o.keyCode === KEY_CODE_DEL) { // delete
-            no_handler = false;
-            const sel = getSelects();
-            if (sel.length) {
-                const next = Data.next(data, (it) => eq.id(it.id, sel[sel.length - 1]));
-
-                doChange(data.filter((it) => !sel.find((sid) => eq.id(it.id, sid))));
-                setShiftSelect([]);
-                setCursor(next ? next.id : 0);
-            } else {
-                const next = Data.next(data, (it) => eq.id(it.id, cursor));
-                doChange(data.filter((it) => !eq.id(it.id, cursor)));
-                setCursor(next ? next.id : 0);
-            }
-        }
-
-        // if ([KEY_CODE_LEFT, KEY_CODE_RIGHT, KEY_CODE_UP, KEY_CODE_DOWN,KEY_].indexOf(o.keyCode) > -1 || isCharKey(o.keyCode)) {
-        // scroll.toViewPort('.editor-area', '.cursor', { margin: 32 });
-        // if (scr) console.log({ scr });
-        // }
-        if (no_handler) {
-            console.log('no handler');
-        }
+        });
         // o.stopPropagation();
         o.preventDefault();
         return false;
--- jsx/js/DataWrap.js
+++ jsx/js/DataWrap.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-return-assign */
 class DataWrap {
     constructor(data = []) {
-        this.data = [];
+        this.data = data;
         this.indexs = {};
         this._locksync = 0;
         this.sync();
@@ -12,7 +12,11 @@ class DataWrap {
     }
 
     itemByIndex(index) {
-        return this.data[index];
+        return index >= 0 && index < this.data.length ? this.data[index] : false;
+    }
+
+    index(id) {
+        return id in this.indexs ? this.indexs[id] : false;
     }
 
     lockSync() {
@@ -27,14 +31,55 @@ class DataWrap {
     }
 
     sync() {
-        if (!this._locksync === 0) {
+        if (this._locksync === 0) {
+            // console.time('sync');
             this.indexs = {};
             this.data.map((it, i) => this.indexs[it.id] = i);
+            // console.timeEnd('sync');
         }
     }
 
     add(data) {
         this.data = [...this.data, ...data];
         this.sync();
+        return this.data;
+    }
+
+    change(data) {
+        this.data = data;
+        this.sync();
+        return this.data;
+    }
+
+    prev(id) {
+        return this.itemByIndex(this.index(id) - 1);
+    }
+
+    next(id) {
+        return this.itemByIndex(this.index(id) + 1);
+    }
+
+    nearest(id, findedCallback, left = true) {
+        const index = this.index(id);
+        let from; let step;
+        if (left) {
+            from = index - 1;
+            step = -1;
+        } else {
+            from = index + 1;
+            step = 1;
+        }
+        let i = from;
+        while (i >= 0 && i < this.data.length) {
+            const it = this.data[i];
+            if (findedCallback(it)) {
+                return it;
+            }
+            i += step;
+        }
+
+        return false;
     }
 }
+
+export default DataWrap;
