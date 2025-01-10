a1583f18883eb85a2a5d80d31095603b133a8e4d
fmihel <fmihel76@gmail.com>
2025-01-10 12:23:06 +0300

    add CssClass test

---
+++ tests/utils/CssClass-test.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-undef */
+import { expect } from 'chai';
+import css from '../../utils/CssClass';
+
+describe('CssClass.have', () => {
+    it('css.have(\'a b c\', \'c\')', () => {
+        const result = css.have('a b c', 'c');
+        const should = true;
+        expect(result).to.equal(should);
+    });
+    it('css.have(\'a b c\', \'c\')', () => {
+        const result = css.have('a b c', 'd');
+        const should = false;
+        expect(result).to.equal(should);
+    });
+    it('css.have("a b c f"," f")', () => {
+        const result = css.have('a b c f', ' f');
+        const should = true;
+        expect(result).to.equal(should);
+    });
+});
+
+describe('CssClass.add', () => {
+    it('css.add("", "c")', () => {
+        const result = css.add('', 'c ');
+        const should = 'c';
+        expect(result).to.equal(should);
+    });
+    it('css.add("m", "c")', () => {
+        const result = css.add('m', 'c ');
+        const should = 'm c';
+        expect(result).to.equal(should);
+    });
+    it('css.add("m a", "c")', () => {
+        const result = css.add('m a', 'c ');
+        const should = 'm a c';
+        expect(result).to.equal(should);
+    });
+});
+
+describe('CssClass.remove', () => {
+    it('css.remove("a c b", "c")', () => {
+        const result = css.remove('a c b', 'c');
+        const should = 'a b';
+        expect(result).to.equal(should);
+    });
+    it('css.remove("a c b d e", "c","d")', () => {
+        const result = css.remove('a c b d e', 'c', 'd');
+        const should = 'a b e';
+        expect(result).to.equal(should);
+    });
+    it('css.remove("a b c", "c","a","b")', () => {
+        const result = css.remove('a   b  c', 'c', 'a', 'b  ');
+        const should = '';
+        expect(result).to.equal(should);
+    });
+});
+
+describe('CssClass.toggle', () => {
+    it('css.toggle("a c b", "c")', () => {
+        const result = css.toggle('a c b', 'c');
+        const should = 'a b';
+        expect(result).to.equal(should);
+    });
+    it('css.toggle("a b", "c")', () => {
+        const result = css.toggle('a b', 'c');
+        const should = 'a b c';
+        expect(result).to.equal(should);
+    });
+});
--- utils/CssClass.js
+++ utils/CssClass.js
@@ -1,21 +1,23 @@
 class CssClass {
     static have(css, className) {
-        return (css || '').split(' ').indexOf(className) >= 0;
+        return this.asArray(css).indexOf(className.trim()) >= 0;
     }
 
     static add(css, ...classNames) {
-        let out = css || '';
+        const out = this.asArray(css);
+
         classNames.map((className) => {
-            if (!this.have(out, className)) {
-                out += ((out ? ' ' : '') + className).trim();
+            if (out.indexOf(className) === -1) {
+                out.push(className.trim());
             }
         });
 
-        return out;
+        return out.join(' ').trim();
     }
 
     static remove(css, ...classNames) {
-        return (css || '').split(' ').filter((it) => (classNames.indexOf(it) === -1)).join(' ').trim();
+        const m = classNames.map((it) => it.trim());
+        return this.asArray(css).filter((it) => (m.indexOf(it.trim()) === -1)).join(' ').trim();
     }
 
     static toggle(css, className) {
@@ -24,6 +26,10 @@ class CssClass {
 
     static eq(className1, className2) {
         return className1.trim() === className2.trim();
+    }
+
+    static asArray(css) {
+        return (css || '').split(' ').map((c) => c.trim()).filter((c) => c);
     }
 }
 
